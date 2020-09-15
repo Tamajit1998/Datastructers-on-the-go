5eef984e28558bae7e06548c09367f9dacfaa038
Tamajit Das <34771271+Tamajit1998@users.noreply.github.com>
2020-09-16 00:06:14 +0530

    Delete selectionsort.h

--- Sorting/selectionsort.h
+++
@@ -1,9 +0,0 @@
-//
-// Created by tamji on 9/15/2020.
-//
-
-#ifndef SELECTIONSORT_SELECTIONSORT_H
-#define SELECTIONSORT_SELECTIONSORT_H
-void selectionsort(int*,int);
-void swap(int*,int*);
-#endif //SELECTIONSORT_SELECTIONSORT_H
